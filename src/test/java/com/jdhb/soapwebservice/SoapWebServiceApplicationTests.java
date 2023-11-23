3e81a86b1b8e4ee5b8f1f075d634651f2550637f
Juan D Hernandez <juandanielhb@gmail.com>
2023-11-22 21:49:31 -0500

    Initial commit - Spring boot base project

---
+++ src/test/java/com/jdhb/soapwebservice/SoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdhb.soapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
